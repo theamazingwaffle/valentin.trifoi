42da50c327187a9b659faed15d8361e1d2513b81
Filipenco Pavel <filipencop@mail.ru>
2021-11-04 15:25:29 +0200

    carousel fix

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,28 @@
 let carousels = document.getElementsByClassName("carousel")
 
+function calculate_pos(images, index) {
+    let sum = 0;
+    for (let i = 0; i < index && i < images.length; i++) {
+        sum += images[i].width;
+    }
+    return sum;
+}
+
 for (let carousel of carousels) {
     carousel.lookingAt = 0;
     carousel.imageCount = carousel.children[2].children.length;
     let images = carousel.children[2].children;
+    let image_container = carousel.children[2]
     carousel.children[0].addEventListener('click', _ => {
         carousel.lookingAt = ((carousel.lookingAt - 1) +
                               carousel.imageCount) % carousel.imageCount;
-        images[carousel.lookingAt].scrollIntoView(
-            {behavior: "smooth", inline: "center", block: "center"});
+        let left = calculate_pos(image_container.children, carousel.lookingAt);
+        image_container.scrollTo({left: left, behavior: "smooth"});
     });
 
     carousel.children[1].addEventListener('click', _ => {
         carousel.lookingAt = (carousel.lookingAt + 1) % carousel.imageCount;
-        images[carousel.lookingAt].scrollIntoView(
-            {behavior: "smooth", inline: "center", block: "center"});
+        let left = calculate_pos(image_container.children, carousel.lookingAt);
+        image_container.scrollTo({left: left, behavior: "smooth"});
     });
 }
